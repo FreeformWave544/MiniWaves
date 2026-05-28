ac073f782384fe763c5a46cb9d83cd03a9809643
FreeformWave544 <165945352+FreeformWave544@users.noreply.github.com>
2026-05-28 23:03:30 +0100

    Cleared up Debug & Improved Shop

--- games/main.gd
+++ games/main.gd
@@ -3,15 +3,12 @@ extends Control
 var score := 0.0
 @onready var oldTime = Time.get_ticks_msec()
 func _on_clicker_pressed() -> void:
-	if (Time.get_ticks_msec() - oldTime) < 80: print("X") ; return
-	print("Y")
+	if (Time.get_ticks_msec() - oldTime) < 80: return
 	oldTime = Time.get_ticks_msec()
 	update()
 	score += Upgrades.upgrades["Click"]
 	update()
 
-func update() -> void:
-	$Score.text = "SCORE:\n%.2f" % score
+func update() -> void: $Score.text = "SCORE:\n%.2f" % score
 
-func _on_shop_pressed() -> void:
-	$ShopWindow.show()
+func _on_shop_pressed() -> void: $ShopWindow.show()
